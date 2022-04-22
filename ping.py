1a43cbeb46ef95771b750abe1eab6351081890d4
Bob Paauwe <bpaauwe@yahoo.com>
2022-04-22 10:25:01 -0700

    Set controller ST driver as connection status

--- ping.py
+++ ping.py
@@ -30,7 +30,7 @@ class Controller(udi_interface.Node):
         polyglot.subscribe(polyglot.POLL, self.poll)
 
         polyglot.ready()
-        polyglot.addNode(self)
+        polyglot.addNode(self, conn_status="ST")
 
     def parameterHandler(self, params):
         for key,val in params.items():
